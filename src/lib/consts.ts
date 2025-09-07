783f145ef048d36982b92c9d214f2317bb81af28
UnderMind <jhoandavidnaranjo@gmail.com>
2025-09-07 16:10:03 -0500

    labels context chat IA & action's description

---
+++ src/lib/consts.ts
@@ -0,0 +1,27 @@
+export const UI_MSG =
+{
+    LOADING: {
+        STORY: 'Cargando Historia...',
+        IMAGE: 'Generando Realidades...',
+
+
+    },
+    ERROR: {
+        STORY_GENERATION: 'Error al cargar la historia...',
+        IMAGE_GENERATION: 'Error al generar esta realidad...',
+        MISSING_PROMPT: 'Falta el Prompt para generar una realidad.'
+    },
+    PLACEHOLDERS: {
+        INPUT: 'Describe qué quieres hacer, Cómo?, qué examinar o como reaccionar...'
+    }
+
+    
+}
+
+export const GAME_Config = 
+{
+    IMAGE: {
+        DEFAULT_PROMPT: 'A dystopian reality, full of anomalies and half-naked wolf women who want to kill you, scene semi-pixel art style.',
+        SEPARATOR: 'IMAGEN:'
+    }
+}
